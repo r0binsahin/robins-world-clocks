0416a43ff6eff5eec77287918813f06315914189
r0binsahin <robin.sahin88@gmail.com>
2024-07-09 15:27:20 +0200

    localstorage populated with initial data

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { useEffect, useState } from 'react';
 import { ClockType } from './models/Clock';
 
 function App() {
+  const initialClocks = [
+    { location: 'Stockholm', timezone: 'Europe/Stockholm' },
+    { location: 'London', timezone: 'Europe/London' },
+  ];
   const [clocks, setClocks] = useState<ClockType[]>(
     JSON.parse(localStorage.getItem('clocks') || '[]')
   );
@@ -18,6 +22,12 @@ function App() {
   };
 
   useEffect(() => {
+    if (!localStorage.getItem('clocks')) {
+      localStorage.setItem('clocks', JSON.stringify(initialClocks));
+      setClocks(initialClocks);
+    }
+  }, []);
+  useEffect(() => {
     localStorage.setItem('clocks', JSON.stringify(clocks));
   }, [clocks]);
   return (
@@ -25,15 +35,16 @@ function App() {
       <h1 className='title'>World Clocks</h1>
       <Board clocks={clocks} />
       <div className='input-box'>
+        <h1>Add more cities</h1>
         <input
           type='text'
-          placeholder='City'
+          placeholder='city'
           value={location}
           onChange={(e) => setLocation(e.target.value)}
         />
         <input
           type='text'
-          placeholder='Timezone'
+          placeholder='timezone'
           value={timezone}
           onChange={(e) => setTimezone(e.target.value)}
         />
