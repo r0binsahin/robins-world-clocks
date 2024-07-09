21673e6926ae2142d598e2b75e94f835c4cb8c45
r0binsahin <robin.sahin88@gmail.com>
2024-07-09 11:24:41 +0200

    clock comp initaltime

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,3 +1,21 @@
-export const Clock = () => {
-  return <div>Clock</div>;
+import { useState } from 'react';
+
+interface ClockProps {
+  location: string;
+  timezone: string;
+}
+
+export const Clock = ({ location, timezone }: ClockProps) => {
+  const initialTime = new Date().toLocaleString('sv', { timeZone: timezone });
+
+  console.log(initialTime);
+
+  const [time, setTime] = useState(initialTime);
+
+  return (
+    <div>
+      <h3>{location}</h3>
+      <p>{time}</p>
+    </div>
+  );
 };
